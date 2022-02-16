2f86d962bb1cf4b7cc3baafbf1470fe417ad4a68
ifahadone <me@ifahad.one>
2022-02-17 04:24:18 +0800

    test file

---
+++ TestHelper.ps1
@@ -0,0 +1,37 @@
+# Unzip Arc
+Expand-Archive -LiteralPath 'C:\W.Zip' -DestinationPath C:\Apps\Logs -Force
+
+# Create Dummy Logs
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowManager-20221110.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher2-20221210.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20221310.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowManager-20221410.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher2-20221510.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20221610.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowManager-20221710.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher2-20221812.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20221912.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowManager-20221012.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher2-20221112.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20221212.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowManager-20221312.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher2-20221412.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20221512.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowManager-20221612.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher2-20221712.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20221812.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowManager-20221912.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher2-20222011.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20222111.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowManager-20222211.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher2-20222311.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20222411.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowManager-20222511.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher2-20222611.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20222711.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowManager-20222811.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher2-20222911.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20223011.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowManager-20221111.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher2-20222311.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20221211.log" -ItemType File
