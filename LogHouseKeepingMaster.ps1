48938bb242fc2408671eb19148fea2cc9dc631de
ifahadone <me@ifahad.one>
2022-02-22 02:40:04 +0800

    Tue Feb 22 02:40:04 +08 2022

--- LogHouseKeepingMaster.ps1
+++ LogHouseKeepingMaster.ps1
@@ -1,51 +1,89 @@
 cd C:\Apps\Logs\
+
+# LOG & ARCHIVE DIRECTORIES
 [string]$LogPath="C:\Apps\Logs\"
-[string]$Logs="C:\Apps\Logs\*.log"
-[string]$LogDelLog="C:\Apps\Logs\DelLog.txt"
 [string]$LogArchivePath="C:\Apps\Logs\Archives\"
-[string]$LogArchiveDelLog="C:\Apps\Logs\Archives\ArchiveDelLog.txt"
-[string]$Today=Get-Date -f yyyyMMdd
 
+# PS LOGS FILES
+[string]$DelLog="C:\Apps\Logs\DelLog.txt"
+[string]$ArchiveDelLog="C:\Apps\Logs\Archives\ArchiveDelLog.txt"
 
+# CHECK IF USER ADMINSTRATOR
+$elevated = ([Security.Principal.WindowsPrincipal] `
+ [Security.Principal.WindowsIdentity]::GetCurrent()
+).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+Write-Host 'Me= $elevated'
+
+## CREATE SCHEDULER
+#[string]$action=New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument $PSCommandPath
+#[string]$trigger=New-ScheduledTaskTrigger -At 01:00am -Daily
+#[string]$settings=New-ScheduledTaskSettingsSet -MultipleInstances Parallel
+#[string]$taskname="LogMaster"
+#[string]$taskdescription="Manage K2 Logs and Archives"
+#Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskname -Description $taskdescription -Settings $settings -User "System"
+#
+## DELETE IF REDUNDANT SCHEDULER EXIST #1
+#[string]$apachelogs="Apache Logs Housekeeping"
+#Get-ScheduledTask -TaskName $apachelogs -ErrorAction SilentlyContinue -OutVariable apachelogstask
+#if ($apachelogstask) {
+#    Get-ScheduledTask -TaskName $apachelogs | Unregister-ScheduledTask -Confirm:$false
+#    }
+#
+## DELETE IF REDUNDANT SCHEDULER EXIST #2
+#[string]$appslogs="Apps Logs Housekeeping"
+#Get-ScheduledTask -TaskName $appslogs -ErrorAction SilentlyContinue -OutVariable appslogstask
+#if ($appslogstask) {
+#    Get-ScheduledTask -TaskName $appslogstask | Unregister-ScheduledTask -Confirm:$false
+#    }
+
+# CREATE IF LOG PATH NOT EXIST
+if(!(Test-Path -Path $LogPath )){
+    try { New-Item -Path $LogPath -ItemType directory } catch { }
+}
+
+# CREATE IF ARCHIVE PATH NOT EXIST
 if(!(Test-Path -Path $LogArchivePath )){
     try { New-Item -Path $LogArchivePath -ItemType directory } catch { }
 }
 
-
-if (Test-Path $LogDelLog) {
-    try { Remove-Item $LogDelLog } catch { }
+# REMOVE PS DEL-LOG
+if (Test-Path $DelLog) {
+    try { Remove-Item $DelLog } catch { }
 }
 
-if (Test-Path $LogArchiveDelLog) {
-    try { Remove-Item $LogArchiveDelLog } catch { }
+# REMOVE PS DEL-ARCHIVE-LOG
+if (Test-Path $ArchiveDelLog) {
+    try { Remove-Item $ArchiveDelLog } catch { }
 }
-
+# FIND LOGS WITH REGEX AND GROUP THEM FOLLOWED BY COMPRESSION
+[string]$Logs="C:\Apps\Logs\*.log"
 dir $Logs | group  { $_.Name -replace '-\d\d\d\d\d\d\d\d.log$', '' } | %{
     $final=$_.Name
     $filename= "Archives\$final-$(Get-Date -f yyyyMMdd).zip"
-    Write-Host "=>"$filename
 
     $subset.Group | %{
     try { Compress-Archive -Path $_ -DestinationPath $filename -Update -CompressionLevel Optimal } catch { }
     }
 }
 
-
+# DELETE LOGS OLDER THAN #1 DAY
+[string]$Today=Get-Date -f yyyyMMdd
 Get-ChildItem $LogPath -Force -ea 0 -Filter *.log |
 ? {!$_.PsIsContainer -and $_.LastWriteTime -lt (Get-Date).AddDays(-1)} |
 Select-Object -Skip 1 |
 ForEach-Object {
     Write-Host "LOG"$_.FullName
     try { $_ | del -Force } catch { }
-    try { "$Today | $_" | Out-File $LogDelLog -Append } catch { }
+    try { "$Today | $_" | Out-File $DelLog -Append } catch { }
 }
 
+# DELETE ARCHIVE OLDER THAN #60 DAY
 Get-ChildItem $LogArchivePath -Recurse -Force -ea 0 -Filter *.zip |
-? {!$_.PsIsContainer -and $_.LastWriteTime -lt (Get-Date).AddDays(0)} |
+? {!$_.PsIsContainer -and $_.LastWriteTime -lt (Get-Date).AddDays(-60)} |
 ForEach-Object {
     Write-Host "ARC"$_.FullName
     try { $_ | del -Force } catch { }
-    try { "$Today | $_" | Out-File $LogArchiveDelLog -Append } catch { }
+    try { "$Today | $_" | Out-File $ArchiveDelLog -Append } catch { }
 }
 
 
--- TestHelper.ps1
+++ TestHelper.ps1
@@ -36,5 +36,74 @@ New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowManager-20222811.log" -ItemType Fi
 New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher2-20222911.log" -ItemType File
 New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20223011.log" -ItemType File
 New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowManager-20221111.log" -ItemType File
-New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher2-20222311.log" -ItemType File
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20222311.log" -ItemType File
 New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20221211.log" -ItemType File
+
+
+# Create Dummy Logs With LastWriteTime (EXPERIMENT)
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowManager-20221110.log" -ItemType File
+(Get-Item "C:\Apps\Logs\WorkFlowManager-20221110.log").LastWriteTime = '01/31/2021 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "MhBosDispather-Test-20221210.log" -ItemType File
+(Get-Item "C:\Apps\Logs\MhBosDispather-Test-20221210.log").LastWriteTime = '02/15/2021 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20221310.log" -ItemType File
+(Get-Item "C:\Apps\Logs\WorkFlowDispatcher3-20221310.log").LastWriteTime = '03/02/2021 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowManager-20221410.log" -ItemType File
+(Get-Item "C:\Apps\Logs\WorkFlowManager-20221410.log").LastWriteTime = '03/17/2021 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "MhBosDispather-Test-20221510.log" -ItemType File
+(Get-Item "C:\Apps\Logs\MhBosDispather-Test-20221510.log").LastWriteTime = '04/01/2021 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20221610.log" -ItemType File
+(Get-Item "C:\Apps\Logs\WorkFlowDispatcher3-20221610.log").LastWriteTime = '04/16/2021 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowManager-20221710.log" -ItemType File
+(Get-Item "C:\Apps\Logs\WorkFlowManager-20221710.log").LastWriteTime = '05/01/2021 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "MhBosDispather-Test-20221812.log" -ItemType File
+(Get-Item "C:\Apps\Logs\MhBosDispather-Test-20221812.log").LastWriteTime = '05/16/2021 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20221912.log" -ItemType File
+(Get-Item "C:\Apps\Logs\WorkFlowDispatcher3-20221912.log").LastWriteTime = '05/31/2021 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowManager-20221012.log" -ItemType File
+(Get-Item "C:\Apps\Logs\WorkFlowManager-20221012.log").LastWriteTime = '06/15/2021 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "MhBosDispather-Test-20221112.log" -ItemType File
+(Get-Item "C:\Apps\Logs\MhBosDispather-Test-20221112.log").LastWriteTime = '06/30/2021 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20221212.log" -ItemType File
+(Get-Item "C:\Apps\Logs\WorkFlowDispatcher3-20221212.log").LastWriteTime = '07/15/2021 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowManager-20221312.log" -ItemType File
+(Get-Item "C:\Apps\Logs\WorkFlowManager-20221312.log").LastWriteTime = '07/30/2021 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "MhBosDispather-Test-20221412.log" -ItemType File
+(Get-Item "C:\Apps\Logs\MhBosDispather-Test-20221412.log").LastWriteTime = '08/14/2021 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20221512.log" -ItemType File
+(Get-Item "C:\Apps\Logs\WorkFlowDispatcher3-20221512.log").LastWriteTime = '08/29/2021 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowManager-20221612.log" -ItemType File
+(Get-Item "C:\Apps\Logs\WorkFlowManager-20221612.log").LastWriteTime = '09/13/2021 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "MhBosDispather-Test-20221712.log" -ItemType File
+(Get-Item "C:\Apps\Logs\MhBosDispather-Test-20221712.log").LastWriteTime = '09/28/2021 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20221812.log" -ItemType File
+(Get-Item "C:\Apps\Logs\WorkFlowDispatcher3-20221812.log").LastWriteTime = '10/13/2021 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowManager-20221912.log" -ItemType File
+(Get-Item "C:\Apps\Logs\WorkFlowManager-20221912.log").LastWriteTime = '10/28/2021 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "MhBosDispather-Test-20222011.log" -ItemType File
+(Get-Item "C:\Apps\Logs\MhBosDispather-Test-20222011.log").LastWriteTime = '11/12/2021 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20222111.log" -ItemType File
+(Get-Item "C:\Apps\Logs\WorkFlowDispatcher3-20222111.log").LastWriteTime = '11/27/2021 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowManager-20222211.log" -ItemType File
+(Get-Item "C:\Apps\Logs\WorkFlowManager-20222211.log").LastWriteTime = '12/12/2021 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "MhBosDispather-Test-20222311.log" -ItemType File
+(Get-Item "C:\Apps\Logs\MhBosDispather-Test-20222311.log").LastWriteTime = '12/27/2021 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20222411.log" -ItemType File
+(Get-Item "C:\Apps\Logs\WorkFlowDispatcher3-20222411.log").LastWriteTime = '01/11/2022 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowManager-20222511.log" -ItemType File
+(Get-Item "C:\Apps\Logs\WorkFlowManager-20222511.log").LastWriteTime = '01/26/2022 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "MhBosDispather-Test-20222611.log" -ItemType File
+(Get-Item "C:\Apps\Logs\MhBosDispather-Test-20222611.log").LastWriteTime = '02/10/2022 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20222711.log" -ItemType File
+(Get-Item "C:\Apps\Logs\WorkFlowDispatcher3-20222711.log").LastWriteTime = '02/25/2022 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowManager-20222811.log" -ItemType File
+(Get-Item "C:\Apps\Logs\WorkFlowManager-20222811.log").LastWriteTime = '03/12/2022 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "MhBosDispather-Test-20222911.log" -ItemType File
+(Get-Item "C:\Apps\Logs\MhBosDispather-Test-20222911.log").LastWriteTime = '03/27/2022 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20223011.log" -ItemType File
+(Get-Item "C:\Apps\Logs\WorkFlowDispatcher3-20223011.log").LastWriteTime = '04/11/2022 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowManager-20221111.log" -ItemType File
+(Get-Item "C:\Apps\Logs\WorkFlowManager-20221111.log").LastWriteTime = '04/26/2022 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20222311.log" -ItemType File
+(Get-Item "C:\Apps\Logs\WorkFlowDispatcher3-20222311.log").LastWriteTime = '04/26/2022 06:00:36'
+New-Item -Path 'C:\Apps\Logs\' -Name "WorkFlowDispatcher3-20221211.log" -ItemType File
+(Get-Item "C:\Apps\Logs\WorkFlowDispatcher3-20221211.log").LastWriteTime = '04/26/2022 06:00:36'
