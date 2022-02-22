af32046e0c6a7b067dc5b74fb75469bc611f47ee
ifahadone <me@ifahad.one>
2022-02-22 23:55:28 +0800

    updated naming convention, skip delete log skip 1 object

--- LogHouseKeepingMaster.ps1
+++ LogHouseKeepingMaster.ps1
@@ -1,96 +1,97 @@
 cd C:\Apps\Logs\
 
 # LOG & ARCHIVE DIRECTORIES
-[string]$LogPath="C:\Apps\Logs\"
-[string]$LogArchivePath="C:\Apps\Logs\Archives\"
+[string]$LOG_PATH = "C:\Apps\Logs\"
+[string]$LOG_ARC_PATH = "C:\Apps\Logs\Archives\"
 
 # PS LOGS FILES
-[string]$DelLog="C:\Apps\Logs\DelLog.txt"
-[string]$ArchiveDelLog="C:\Apps\Logs\Archives\ArchiveDelLog.txt"
+[string]$LOGGER_DEL = "C:\Apps\Logs\DelLog.txt"
+[string]$LOGGER_ARC_DEL = "C:\Apps\Logs\Archives\ArchiveDelLog.txt"
 
 # CHECK IF USER ADMINSTRATOR
-$elevated = ([Security.Principal.WindowsPrincipal] `
- [Security.Principal.WindowsIdentity]::GetCurrent()
-).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+$elevated = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 Write-Host "Me= $elevated"
 
-# CREATE SCHEDULER
-$TRIGGER = New-ScheduledTaskTrigger -At 01:00am -Daily
-$USER = "System"
-$ACTION = New-ScheduledTaskAction powershell.exe -Argument “-file C:\Users\fahad\Repos\LogCompresser.ps1" 
-$TASK_NAME="LogMaster"
-$TASK_DESC="Manage K2 Logs and Archives"
-Register-ScheduledTask -TaskName $TASK_NAME -Description $TASK_DESC -Trigger $TRIGGER -User $USER -Action $ACTION
-
-
-
-
-
-# DELETE IF REDUNDANT SCHEDULER EXIST #1
-[string]$apachelogs="Apache Logs Housekeeping"
-Get-ScheduledTask -TaskName $apachelogs -ErrorAction SilentlyContinue -OutVariable apachelogstask
-if ($apachelogstask) {
-    Get-ScheduledTask -TaskName $apachelogs | Unregister-ScheduledTask -Confirm:$false
+if ($elevated) {
+    # CREATE SCHEDULER
+    $Trigger = New-ScheduledTaskTrigger -At 01:00am -Daily
+    $Action = New-ScheduledTaskAction powershell.exe -Argument “-file $PSCommandPath" 
+    [string]$User = "System"
+    [string]$Task_Name = "LogMaster"
+    [string]$Task_Desc = "Manage K2 Logs and Archives"
+    Get-ScheduledTask -TaskName $Task_Name -ErrorAction SilentlyContinue -OutVariable isLogMaster
+    Write-Host "isLogMaster" $isLogMaster
+    if (!$isLogMaster) {
+        Register-ScheduledTask -TaskName $Task_Name -Description $Task_Desc -Trigger $Trigger -User $User -Action $Action
     }
 
-# DELETE IF REDUNDANT SCHEDULER EXIST #2
-[string]$appslogs="Apps Logs Housekeeping"
-Get-ScheduledTask -TaskName $appslogs -ErrorAction SilentlyContinue -OutVariable appslogstask
-if ($appslogstask) {
-    Get-ScheduledTask -TaskName $appslogstask | Unregister-ScheduledTask -Confirm:$false
+    # DELETE IF REDUNDANT SCHEDULER EXIST #1
+    [string]$Sched_Apachelogs = "Apache Logs Housekeeping"
+    Get-ScheduledTask -TaskName $Sched_Apachelogs -ErrorAction SilentlyContinue -OutVariable isSchedApachelogs
+    Write-Host "isSchedApachelogs" $isSchedApachelogs
+    if ($isSchedApachelogs) {
+        Get-ScheduledTask -TaskName $Sched_Apachelogs | Unregister-ScheduledTask -Confirm:$false
     }
+
+    # DELETE IF REDUNDANT SCHEDULER EXIST #2
+    [string]$Sched_Appslogs = "Apps Logs Housekeeping"
+    Get-ScheduledTask -TaskName $Sched_Appslogs -ErrorAction SilentlyContinue -OutVariable isSchedAppslogs
+    Write-Host "isSchedAppslogs" $isSchedAppslogs
+    if ($isSchedAppslogs) {
+        Get-ScheduledTask -TaskName $Sched_Appslogs | Unregister-ScheduledTask -Confirm:$false
+    }
+}
 
 # CREATE IF LOG PATH NOT EXIST
-if(!(Test-Path -Path $LogPath )){
-    try { New-Item -Path $LogPath -ItemType directory } catch { }
+if (!(Test-Path -Path $LOG_PATH )) {
+    try { New-Item -Path $LOG_PATH -ItemType directory } catch { }
 }
 
 # CREATE IF ARCHIVE PATH NOT EXIST
-if(!(Test-Path -Path $LogArchivePath )){
-    try { New-Item -Path $LogArchivePath -ItemType directory } catch { }
+if (!(Test-Path -Path $LOG_ARC_PATH )) {
+    try { New-Item -Path $LOG_ARC_PATH -ItemType directory } catch { }
 }
 
 # REMOVE PS DEL-LOG
-if (Test-Path $DelLog) {
-    try { Remove-Item $DelLog } catch { }
+if (Test-Path $LOGGER_DEL) {
+    try { Remove-Item $LOGGER_DEL } catch { }
 }
 
 # REMOVE PS DEL-ARCHIVE-LOG
-if (Test-Path $ArchiveDelLog) {
-    try { Remove-Item $ArchiveDelLog } catch { }
+if (Test-Path $LOGGER_ARC_DEL) {
+    try { Remove-Item $LOGGER_ARC_DEL } catch { }
 }
 # FIND LOGS WITH REGEX AND GROUP THEM FOLLOWED BY COMPRESSION
-[string]$Logs="C:\Apps\Logs\*.log"
-[string]$Today=Get-Date -f _yyyy-MM-dd
-dir $Logs | group  { $_.Name -replace '-\d\d\d\d\d\d\d\d.log$', ''} | %{
+[string]$Logs = "C:\Apps\Logs\*.log"
+[string]$Today = Get-Date -f _yyyy-MM-dd
+dir $Logs | group { $_.Name -replace '-\d\d\d\d\d\d\d\d.log$', '' } | % {
     $subset = $_
-    $finalName= $LogArchivePath+$subset.Name+$Today
+    $finalName = $LOG_ARC_PATH + $subset.Name + $Today
     Write-Host $subset.Name
 
-    $subset.Group | %{
+    $subset.Group | % {
         #$error.clear()
         Compress-Archive $_ $finalName -Update -CompressionLevel Optimal
     }
 }
 
 # DELETE LOGS OLDER THAN #1 DAY
-[string]$Today=Get-Date -f yyyyMMdd
-Get-ChildItem $LogPath -Force -ea 0 -Filter *.log |
-? {!$_.PsIsContainer -and $_.LastWriteTime -lt (Get-Date).AddDays(-1)} |
-Select-Object -Skip 1 |
+[string]$Today = Get-Date -f yyyyMMdd
+Get-ChildItem $LOG_PATH -Force -ea 0 -Filter *.log |
+? { !$_.PsIsContainer -and $_.LastWriteTime -lt (Get-Date).AddDays(-1) } |
 ForEach-Object {
     Write-Host "LOG"$_.FullName
     try { $_ | del -Force } catch { }
-    try { "$Today | $_" | Out-File $DelLog -Append } catch { }
+    try { "$Today | $_" | Out-File $LOGGER_DEL -Append } catch { }
 }
 
 # DELETE ARCHIVE OLDER THAN #60 DAY
-Get-ChildItem $LogArchivePath -Recurse -Force -ea 0 -Filter *.zip |
-? {!$_.PsIsContainer -and $_.LastWriteTime -lt (Get-Date).AddDays(-60)} |
+Get-ChildItem $LOG_ARC_PATH -Recurse -Force -ea 0 -Filter *.zip |
+? { !$_.PsIsContainer -and $_.LastWriteTime -lt (Get-Date).AddDays(-60) } |
 ForEach-Object {
     Write-Host "ARC"$_.FullName
     try { $_ | del -Force } catch { }
-    try { "$Today | $_" | Out-File $ArchiveDelLog -Append } catch { }
+    try { "$Today | $_" | Out-File $LOGGER_ARC_DEL -Append } catch { }
 }
 
 
